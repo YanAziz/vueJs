27ea8cb40fee4a186237b5fc0b21f8c4e51dbc95
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2024-01-19 02:57:52 +0700

    methods-computed

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
+// var app = new Vue({
+//   el: "#app",
+//   data: {
+//     maximum: 50,
+//     products: null,
+//     cart: [],
+//   },
+//   mounted: function () {
+//     fetch("https://hplussport.com/api/products/order/price")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         this.products = data;
+//       });
+//   },
+//   methods: {
+//     addItem: function (product) {
+//       this.cart.push(product);
+//     },
+//   },
+// });
+
 var app = new Vue({
   el: "#app",
   data: {
-    maximum: 50,
-    products: null,
-    cart: [],
+    slugText: "The Quick #(*!&@ Brown 29",
   },
-  mounted: function () {
-    fetch("https://hplussport.com/api/products/order/price")
-      .then((response) => response.json())
-      .then((data) => {
-        this.products = data;
-      });
+  computed: {
+    // now: function () {
+    //     var date = new Date();
+    //     return (
+    //         String(date.getHours()) +
+    //         String(date.getMinutes()) +
+    //         String(date.getSeconds())
+    //     )
+    // },
+
+    slugetize: function () {
+      return (
+        this.slugText
+          .toLowerCase()
+          .replace(/[^\w ]+/g, "")
+          .replace(/ +/g, "-") +
+        "-" +
+        this.now()
+      );
+    },
   },
+
   methods: {
-    addItem: function (product) {
-      this.cart.push(product);
+    now: function () {
+      var date = new Date();
+      return String(date.getHours()) + String(date.getMinutes()) + String(date.getSeconds());
     },
   },
 });
